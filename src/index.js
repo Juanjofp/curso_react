a3d269f77644e47e391295fbc5a346b6bcf3b3f8
Juanjo Franco <juanjo@juanjofp.com>
2019-03-11 13:16:45 +0100

    fix style.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles.css';
 import App from './app';
 import * as serviceWorker from './serviceWorker';
 
