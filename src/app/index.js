898c104fd4b3285674175b2572f900eedd803cce
Juanjo Franco <juanjo@juanjofp.com>
2019-03-11 15:29:02 +0100

    expression in jsx

--- src/app/index.js
+++ src/app/index.js
@@ -2,11 +2,16 @@
 import React, { Component } from 'react';
 import './styles.css';
 
+function formatName(user) {
+    return <p>{user.name + ' ' + user.lastname}</p>
+}
 class App extends Component {
     render() {
+        const user = {name: 'Juanjo', lastname: 'Franco'}
+        const message = <span>Hello {formatName(user)}</span>
         return (
             <div className="App">
-                Hello World!
+                {message}
             </div>
         );
     }
