13c6359d51aa27129c54a5ef0d067eee383217bc
Juanjo Franco <juanjo@juanjofp.com>
2019-03-11 16:58:21 +0100

    Ejercicio resuleto

--- src/app/index.js
+++ src/app/index.js
@@ -2,20 +2,75 @@
 import React, { Component } from 'react';
 import './styles.css';
 
-class App extends Component {
+// Componente tipo función
+function Welcome(props){
+    // props contiene {name: 'Juanjo'}
+    return (
+        <div>Welcome {props.name} to ReactJS!</div>
+    );
+}
 
+class Fecha extends Component {
     render() {
-        const count = parseInt(Date.now() / 1000);
-        if(count % 2 === 0) {
-            return (
-                <div className="App">
-                Soy un numero par {count}
-                </div>
-            );
-        }
         return (
-            <div className="App">
-                Soy un numero impar {count}
+            <div className='Fecha'>{this.props.fecha}</div>
+        );
+    }
+}
+class Hora extends Component {
+    render() {
+        return (
+            <div className='Hora'>{this.props.hora}</div>
+        );
+    }
+}
+
+function RelojDigital(props) {
+    return (
+        <div className='RelojDigital'>
+            <div className='Timezone'>{props.name}</div>
+            <Fecha fecha={props.fecha}/>
+            <Hora hora={props.hora}/>
+        </div>
+    );
+}
+
+function Almanaque(props) {
+    const fechaCompleta = new Date();
+    const fecha = fechaCompleta.toLocaleDateString();
+    const hora = fechaCompleta.toLocaleTimeString();
+
+    const fechaNY = fechaCompleta.toLocaleDateString("en-US", {timeZone: "America/New_York"});
+    const horaNY = fechaCompleta.toLocaleTimeString("en-US", {timeZone: "America/New_York"});
+
+    return (
+        <div className='Almanaque'>
+            <RelojDigital
+                name='Murcia'
+                fecha={fecha}
+                hora={hora}/>
+            <RelojDigital
+                name='New York'
+                fecha={fechaNY}
+                hora={horaNY}/>
+        </div>
+    );
+}
+
+// class Welcome extends Component {
+//     // this.props contiene {name: 'Juanjo'}
+//     render() {
+//         return <div>Welcome {this.props.name} to ReactJS!</div>;
+//     }
+// }
+
+// Componente tipo Clase
+class App extends Component {
+    render() {
+        return (
+            <div className='App'>
+                <Welcome
+                    name='Juanjo'/>
             </div>
         );
     }
