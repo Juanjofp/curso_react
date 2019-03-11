c27c5b8df281001236cd260c418592b85e5e900e
Juanjo Franco <juanjo@juanjofp.com>
2019-03-11 18:09:59 +0100

    Refactor almanaque

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,6 @@
 // src/app/index.js
 import React, { Component } from 'react';
+import Almanaque from '../almanaque';
 import './styles.css';
 
 // Componente tipo función
@@ -7,76 +8,6 @@ function Welcome(props){
     // props contiene {name: 'Juanjo'}
     return (
         <div>Welcome {props.name} to ReactJS!</div>
-    );
-}
-
-class Fecha extends Component {
-    render() {
-        return (
-            <div className='Fecha'>{this.props.fecha}</div>
-        );
-    }
-}
-class Hora extends Component {
-    render() {
-        return (
-            <div className='Hora'>{this.props.hora}</div>
-        );
-    }
-}
-
-function calculateDate(country, timeZone) {
-    const fechaCompleta = new Date();
-    const fecha = fechaCompleta.toLocaleDateString(country, {timeZone: timeZone});
-    const hora = fechaCompleta.toLocaleTimeString(country, {timeZone: timeZone});
-    return {fecha, hora};
-}
-
-class RelojDigital extends Component {
-    constructor(props) {
-        super(props);
-        // Establecemos el state del componente
-        this.state = calculateDate(this.props.country, this.props.timeZone);
-    }
-
-    componentDidMount() {
-        this.intervalId = setInterval(
-            () => this.setState(calculateDate(this.props.country, this.props.timeZone))
-        );
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.intervalId);
-    }
-    
-    
-    render() {
-        return (
-            <div className='RelojDigital'>
-                <div className='Timezone'>{this.props.name}</div>
-                <Fecha fecha={this.state.fecha}/>
-                <Hora hora={this.state.hora}/>
-            </div>
-        );
-    }
-}
-
-function Almanaque(props) {
-    return (
-        <div className='Almanaque'>
-            <RelojDigital
-                name='Murcia'
-                country='es-ES'
-                timeZone='Europe/Madrid'/>
-            <RelojDigital
-                name='New York'
-                country='en-US'
-                timeZone='America/New_York'/>
-            <RelojDigital
-                name='Tazmania'
-                country='au-AU'
-                timeZone='Australia/Hobart'/>
-        </div>
     );
 }
 
